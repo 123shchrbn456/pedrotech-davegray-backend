a148141e42368c4f893a73d5ad57cd6e3c8b62f3
Igor <qwerty2244668800@gmail.com>
2024-03-16 22:08:11 +0200

    changes for vercel deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,11 @@
 const allowedOrigins = [
+    "http://localhost:5173",
+    // Render com links below
     "https://davegray-pedrotechh-mern-deploy-frontent.onrender.com",
     "http://davegray-pedrotechh-mern-deploy-frontent.onrender.com",
+    // Vercel links below
+    "https://pedrotech-davegray-mern-frontend.vercel.app",
+    "http://pedrotech-davegray-mern-frontend.vercel.app",
 ];
 
 module.exports = allowedOrigins;
